a166e656e209525d3032894c923b8664a747ae06
Michael Kohl <citizen428@forem.com>
2021-10-05 13:52:09 +0700

    Add Cypress tests (WIP)

--- cypress/integration/seededFlows/publishingFlows/addTagsToArticle.spec.js
+++ cypress/integration/seededFlows/publishingFlows/addTagsToArticle.spec.js
@@ -1,8 +1,8 @@
 describe('Add tags to article', () => {
   const exampleTopTags = [
-    { name: 'tagone' },
+    { name: 'tag1' },
     {
-      name: 'tagtwo',
+      name: 'tag2',
       rules_html:
         '<p>Here are some rules <a href="//www.test.com">link here</a></p>\n',
     },
@@ -21,9 +21,9 @@ describe('Add tags to article', () => {
     cy.intercept('/tags/suggest', exampleTopTags);
     cy.findByRole('textbox', { name: 'Post Tags' }).focus();
 
-    cy.findByRole('button', { name: 'tagone' }).should('exist');
+    cy.findByRole('button', { name: 'tag1' }).should('exist');
     cy.findByRole('button', {
-      name: 'tagtwo',
+      name: 'tag2',
     }).should('exist');
   });
 
@@ -32,27 +32,44 @@ describe('Add tags to article', () => {
     cy.findByRole('textbox', { name: 'Post Tags' }).clear().type('something,');
 
     // Search is in progress, top tags which don't match shouldn't be shown
-    cy.findByRole('button', { name: 'tagone' }).should('not.exist');
+    cy.findByRole('button', { name: 'tag1' }).should('not.exist');
     cy.findByRole('button', {
-      name: 'tagtwo',
+      name: 'tag2',
     }).should('not.exist');
 
     // Users initiating fresh search after comma
     cy.findByRole('textbox', { name: 'Post Tags' }).focus();
-    cy.findByRole('button', { name: 'tagone' }).should('exist');
+    cy.findByRole('button', { name: 'tag1' }).should('exist');
     cy.findByRole('button', {
-      name: 'tagtwo',
+      name: 'tag2',
     }).should('exist');
   });
 
   it("doesn't suggest top tags already added", () => {
     cy.intercept('/tags/suggest', exampleTopTags);
     cy.findByRole('textbox', { name: 'Post Tags' }).focus();
-    cy.findByRole('button', { name: 'tagone' }).click();
+    cy.findByRole('button', { name: 'tag1' }).click();
 
-    cy.findByRole('button', { name: 'tagone' }).should('not.exist');
+    cy.findByRole('button', { name: 'tag1' }).should('not.exist');
     cy.findByRole('button', {
-      name: 'tagtwo',
+      name: 'tag2',
     }).should('exist');
+  });
+
+  // Regression specs for #14867
+  it('keeps on suggesting a tag even if we typed in the whole name', () => {
+    cy.findByRole('textbox', { name: 'Post Tags' }).clear().type('tag1');
+
+    // Search is in progress, both tags should still be shown
+    cy.findByRole('button', { name: 'tag1' }).should('exist');
+    cy.findByRole('button', { name: 'tag2' }).should('not.exist');
+  });
+
+  it('no longer suggests a tag once it has been entered', () => {
+    cy.findByRole('textbox', { name: 'Post Tags' }).clear().type('tag1, ');
+
+    // Search is in progress, but java already added
+    cy.findByRole('button', { name: 'tag1' }).should('not.exist');
+    cy.findByRole('button', { name: 'tag2' }).should('exist');
   });
 });
